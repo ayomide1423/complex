8670942442b0559a5ca3f6fefeefc148008451fd
ayomide orojo <orojo.ayomide14@gmail.com>
2022-02-06 09:25:18 -0500

    first

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Fib Calculator With React
+            Fib Calculator With React Version 2
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
